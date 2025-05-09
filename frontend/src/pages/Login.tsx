7399fa2d66288ad9f02b692c22e4af969823152d
mateusribeirocampos <mateusrcampos@gmail.com>
2025-05-09 16:16:28 -0300

    fixed: login updated and test with supabase worked

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -47,7 +47,7 @@ export function Login() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
               </svg>
             </div>
-            <h1 className="text-2xl font-bold text-blue-600"> 2ª PJ de Ouro Fino/MG <br />Sitema Interno</h1>
+            <h1 className="text-3xl font-bold text-blue-600"> 2ª PJ de Ouro Fino/MG <br />Sistema Interno</h1>
             <h2 className="mt-2 text-xl font-semibold text-gray-900">Acesse sua conta</h2>
             <p className="mt-1 text-sm text-gray-500">
               Entre com suas credenciais para acessar o sistema
@@ -133,7 +133,7 @@ export function Login() {
               </div>
               <div className="relative flex justify-center text-sm">
                 <span className="px-2 bg-white text-gray-500">
-                  Sistema Interno SYSMP
+                  Sistema Interno SYSMP2OF
                 </span>
               </div>
             </div>
